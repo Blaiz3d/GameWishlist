48280246698a9a4b30fd2d04f18683d1e92dde21
Mantas V <mantas.visockis0@gmail.com>
2018-07-09 18:45:24 +0300

    Wish list check
    Checks if a game is in the wishlist before adding it.

--- src/com/wishlist/wish/Main.java
+++ src/com/wishlist/wish/Main.java
@@ -37,7 +37,6 @@ public class Main extends Application {
         writeToFile();
     }
 
-
     public static void main(String[] args) throws IOException, FontFormatException
     {
         downloadGameDatabase();
