292b3be9d19f2a6eb614dfa5c2573cec977cb5c4
Mantas V <mantas.visockis0@gmail.com>
2018-07-10 15:38:28 +0300

    Buttons
    Added non-functioning buttons

--- src/com/wishlist/wish/Main.java
+++ src/com/wishlist/wish/Main.java
@@ -47,3 +47,27 @@ public class Main extends Application {
         launch(args);
     }
 }
+//Todos
+//todo: Add ability to add custom games (not from the list)
+//todo: Create About dialog information
+//todo: Add ability to delete all games from wish list
+//todo: Filter wish listed games based on date added
+//todo: Add an icon to the app window
+//todo: Colour code games based on date added
+//todo: Do some css styling
+//todo: Add links to quickly open main game sites (Steam, origin, etc.);
+//todo: Add a search bar for the wish listed games
+//todo: Add a custom font
+
+//Bugfixes
+//todo: Remove context menu from the headers of TreeTableViews
+
+//Future todos
+//todo: Add game images alongside the titles
+
+//Potential todos
+//todo: Find a way to get release dates for games
+//todo: Find a way to get ratings for games
+//todo: Find a way to get tags for games
+//todo: Convert to a web based app, utilize an actual database
+//todo: Remove some entries from the game list (non-game service apps with no real downloads, certain DLCS, packs, etc.)
